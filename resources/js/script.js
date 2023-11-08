803f2d0ed9b36f8ab53e6c2c9dc796d632680af1
Leonardo Aragão <lewoaragao@gmail.com>
2023-11-08 15:27:42 -0300

    [fix] update code

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,5 +1,40 @@
-const card = document.getElementById("card");
+const taskInput = document.getElementById("taskInput");
+const addTaskButton = document.getElementById("addTask");
+const taskList = document.getElementById("taskList");
 
-card.addEventListener("click", function () {
-  card.classList.toggle("flipped");
+addTaskButton.addEventListener("click", function () {
+  const taskText = taskInput.value.trim();
+
+  if (taskText !== "") {
+    const li = document.createElement("li");
+
+    li.innerHTML = `
+              <input type="checkbox" class="checkbox">
+              
+              <span>${taskText}</span>
+
+              <button class="delete">
+                x
+              </button>
+          `;
+
+    taskList.appendChild(li);
+    taskInput.value = "";
+  } else {
+    alert("Enter a task");
+  }
+});
+
+taskList.addEventListener("click", function (event) {
+  if (event.target.classList.contains("delete")) {
+    event.target.parentElement.remove();
+  } else if (event.target.classList.contains("checkbox")) {
+    const task = event.target.nextElementSibling;
+
+    if (event.target.checked) {
+      task.classList.add("complete");
+    } else {
+      task.classList.remove("complete");
+    }
+  }
 });
